8533f0b02abb03a1f1bd299974fbbcb815d64163
r0n9 <fanrong330@gmail.com>
2016-10-07 10:39:25 +0800

    init import

---
+++ engine/src/main/java/vip/fanrong/api/Baby.java
@@ -0,0 +1,12 @@
+package vip.fanrong.api;
+
+import vip.fanrong.REST.REST.Method;
+import vip.fanrong.REST.REST.Service;
+
+@Service
+public class Baby {
+    @Method
+    public static void test() {
+
+    }
+}
